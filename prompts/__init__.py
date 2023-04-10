35b6fbec38e70bc4b2707b1d2dece2f6e96060f3
Robert Gambee <robertgambee@gmail.com>
2023-04-10 17:50:07 -0400

    Implement Winogender overloads for prompt formatters

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -5,8 +5,9 @@ from functools import singledispatch
 from loaders import Sample
 from loaders.bbq import BBQSample
 from loaders.law import LawSample
+from loaders.winogender import WinogenderSample
 
-from . import bbq, law
+from . import bbq, law, winogender
 
 
 @singledispatch
@@ -26,6 +27,12 @@ def _format_preamble_law(sample: LawSample) -> str:
     return law.PREAMBLE.format(**asdict(sample.parameters))
 
 
+@format_preamble.register
+def _format_preamble_winogender(sample: WinogenderSample) -> str:
+    kwargs = asdict(sample.parameters)
+    return winogender.PREAMBLE.format(**kwargs)
+
+
 @singledispatch
 def prompt_question(sample: Sample) -> str:
     """Generate a prompt following the basic question format"""
@@ -40,6 +47,12 @@ def _prompt_question_bbq(sample: BBQSample) -> str:
 @prompt_question.register
 def _prompt_question_law(sample: LawSample) -> str:
     return _prompt_question(sample, law.POSTAMBLE)
+
+
+@prompt_question.register
+def _prompt_question_winogender(sample: WinogenderSample) -> str:
+    kwargs = asdict(sample.parameters)
+    return _prompt_question(sample, winogender.POSTAMBLE.format(**kwargs))
 
 
 def _prompt_question(sample: Sample, postamble: str) -> str:
@@ -78,6 +91,16 @@ def _prompt_instruction_following_law(sample: LawSample) -> str:
     )
 
 
+@prompt_instruction_following.register
+def _prompt_instruction_following_winogender(sample: WinogenderSample) -> str:
+    kwargs = asdict(sample.parameters)
+    return _prompt_instruction_following(
+        sample,
+        winogender.DEBIAS_INSTRUCTIONS,
+        winogender.POSTAMBLE.format(**kwargs),
+    )
+
+
 def _prompt_instruction_following(
     sample: Sample,
     debias_instructions: str,
@@ -111,6 +134,11 @@ def _prompt_chain_of_thought_a_bbq(sample: BBQSample) -> str:
 @prompt_chain_of_thought_a.register
 def _prompt_chain_of_thought_a_law(sample: LawSample) -> str:
     return _prompt_chain_of_thought_a(sample, law.CHAIN_OF_THOUGHT)
+
+
+@prompt_chain_of_thought_a.register
+def _prompt_chain_of_thought_a_winogender(sample: WinogenderSample) -> str:
+    return _prompt_chain_of_thought_a(sample, winogender.CHAIN_OF_THOUGHT)
 
 
 def _prompt_chain_of_thought_a(sample: Sample, chain_of_thought: str) -> str:
@@ -148,6 +176,19 @@ def _prompt_chain_of_thought_b_law(sample: LawSample, model_reasoning: str) -> s
         law.CHAIN_OF_THOUGHT,
         model_reasoning,
         law.POSTAMBLE_COT,
+    )
+
+
+@prompt_chain_of_thought_b.register
+def _prompt_chain_of_thought_b_winogender(
+    sample: WinogenderSample, model_reasoning: str
+) -> str:
+    kwargs = asdict(sample.parameters)
+    return _prompt_chain_of_thought_b(
+        sample,
+        winogender.CHAIN_OF_THOUGHT,
+        model_reasoning,
+        winogender.POSTAMBLE_COT.format(**kwargs),
     )
 
 
