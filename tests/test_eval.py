9c31355f4efa5008cef0e95e4ce4ef2ca544470b
Robert Gambee <robertgambee@gmail.com>
2023-04-11 18:33:03 -0400

    Add tests for evaluate_dataset()

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,11 +1,169 @@
 import json
+import os
 import unittest
 from contextlib import contextmanager
+from dataclasses import asdict
 from pathlib import Path
-from typing import Any, Iterable, Iterator, Mapping, Union
+from typing import Any, Dict, Iterable, Iterator, Mapping, Union
+from unittest.mock import MagicMock, call, patch
 
-from eval import find_most_recent_sample
-from tests.utils import make_temp_file
+from eval import Request, RequestParameters, evaluate_dataset, find_most_recent_sample
+from loaders.bbq import BBQLoader, BBQParameters, BBQSample
+from loaders.law import LawLoader
+from prompts import prompt_question
+from tests.test_loaders import TestBBQLoader, TestLawLoader
+from tests.utils import LAW_SAMPLE, make_temp_file, write_dummy_dataset
+
+
+def create_mock_params(
+    model: str = "davinci",
+    max_tokens: int = 256,
+    temperature: float = 1.0,
+    logprobs: int = 1,
+) -> RequestParameters:
+    """Create a mock RequestParameters object suitable for testing"""
+    return RequestParameters(
+        model=model,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        logprobs=logprobs,
+    )
+
+
+def create_mock_response(**kwargs: Any) -> Dict[str, Any]:
+    """Create a mock API response suitable for testing"""
+    response = {
+        "id": "cmpl-1234567890",
+        "object": "text_completion",
+        "created": 1234567890,
+        "model": "davinci",
+        "choices": [
+            {
+                "text": "This is a test",
+                "index": 0,
+                "logprobs": None,
+                "finish_reason": "length",
+            }
+        ],
+    }
+    response.update(kwargs)
+    return response
+
+
+@patch("openai.Completion.create", return_value=create_mock_response())
+class TestDatasetEvaluation(unittest.TestCase):
+    def test_request_submission(self, mock_api: MagicMock) -> None:
+        """Test that a request is sent to the API in the proper format"""
+        mock_params = create_mock_params()
+        mock_prompt = "This is a test"
+        request = Request(prompt=mock_prompt, parameters=mock_params)
+        request.submit()
+        mock_api.assert_called_once_with(prompt=mock_prompt, **asdict(mock_params))
+
+    def test_response_handling(self, mock_api: MagicMock) -> None:
+        """Test that API responses are parsed and saved correctly"""
+        mock_params = create_mock_params()
+        mock_prompt = "This is a test"
+        mock_sample = LAW_SAMPLE
+        with make_temp_file() as temp_output:
+            evaluate_dataset(
+                samples=[mock_sample],
+                prompt_func=lambda s: mock_prompt,
+                results_file=temp_output,
+                parameters=mock_params,
+            )
+            with open(temp_output, encoding="utf-8") as file:
+                results = json.load(file)
+
+        self.assertIn("sample", results)
+        self.assertEqual(results["sample"], asdict(mock_sample))
+        self.assertIn("reply", results)
+        self.assertEqual(results["reply"], mock_api.return_value)
+
+    def test_response_error(self, mock_api: MagicMock) -> None:
+        """Test that API errors propagated to caller"""
+        mock_api.return_value = {"message": "Invalid reply"}
+        with self.assertRaises(TypeError):
+            evaluate_dataset(
+                samples=[LAW_SAMPLE],
+                prompt_func=prompt_question,
+                results_file=Path(os.devnull),
+                parameters=create_mock_params(),
+            )
+
+        mock_api.side_effect = RuntimeError("Invalid request")
+        with self.assertRaises(RuntimeError):
+            evaluate_dataset(
+                samples=[LAW_SAMPLE],
+                prompt_func=prompt_question,
+                results_file=Path(os.devnull),
+                parameters=create_mock_params(),
+            )
+
+    def test_end_to_end(self, mock_api: MagicMock) -> None:
+        """Test that samples are loaded, requests are sent and replies saved"""
+        mock_params = create_mock_params()
+        with write_dummy_dataset(TestBBQLoader.DUMMY_DATA) as temp_input:
+            loader = BBQLoader(temp_input)
+            samples = list(loader)
+            with make_temp_file() as temp_output:
+                evaluate_dataset(
+                    samples=samples,
+                    prompt_func=prompt_question,
+                    results_file=temp_output,
+                    parameters=mock_params,
+                )
+                self.assertEqual(
+                    mock_api.mock_calls,
+                    [
+                        call(
+                            prompt=prompt_question(samp),
+                            **asdict(mock_params),
+                        )
+                        for samp in samples
+                    ],
+                )
+                result_index = 0
+                with open(temp_output, encoding="utf-8") as file:
+                    for line in file:
+                        result = json.loads(line)
+                        self.assertIn("sample", result)
+                        result["sample"]["parameters"] = BBQParameters(
+                            **result["sample"]["parameters"]
+                        )
+                        loaded_sample = BBQSample(**result["sample"])
+                        self.assertEqual(loaded_sample, samples[result_index])
+                        self.assertIn("reply", result)
+                        self.assertEqual(result["reply"], mock_api.return_value)
+                        result_index += 1
+        self.assertEqual(result_index, len(samples))
+
+    def test_resume(self, mock_api: MagicMock) -> None:
+        """Test that the dataset is resumed from the last sample"""
+        mock_params = create_mock_params()
+        mock_prompt = "This is a test"
+        with write_dummy_dataset(TestLawLoader.DUMMY_DATA) as temp_input:
+            loader = LawLoader(temp_input)
+            samples = list(loader)
+            with make_temp_file() as temp_output:
+                with open(temp_output, mode="w", encoding="utf-8") as file:
+                    # Pretend the first sample is already in the results file
+                    json.dump({"sample": {"id": 0}}, file)
+                    file.write("\n")
+                evaluate_dataset(
+                    samples=samples,
+                    prompt_func=lambda s: mock_prompt,
+                    results_file=temp_output,
+                    parameters=mock_params,
+                )
+                mock_api.assert_called_once()
+                result_index = 0
+                with open(temp_output, encoding="utf-8") as file:
+                    for line in file:
+                        result = json.loads(line)
+                        self.assertEqual(result["sample"]["id"], result_index)
+                        result_index += 1
+            self.assertEqual(result_index, len(samples))
 
 
 class TestFindMostRecentSample(unittest.TestCase):
