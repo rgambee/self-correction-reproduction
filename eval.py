870647b23a82fe60f58560a4b2aeb8d89eeaf61b
Robert Gambee <robertgambee@gmail.com>
2023-04-11 11:37:07 -0400

    Add classes and functions to evalulate samples

---
+++ eval.py
@@ -0,0 +1,122 @@
+from dataclasses import asdict, dataclass
+from pathlib import Path
+from typing import Any, Callable, Dict, Optional, Sequence
+
+import jsonlines
+import openai
+
+from loaders import DatasetLoader, P, Sample
+
+
+@dataclass
+class Completion:
+    """A single completion from the OpenAI API"""
+
+    text: str
+    index: int
+    logprobs: Dict[str, float]
+    finish_reason: str
+
+
+@dataclass
+class Reply:
+    """A reply from the OpenAI API, possibly containing several completions"""
+
+    # pylint doesn't like two letter names, claiming they don't conform to the
+    # snake_case convention
+    id: str  # pylint: disable=invalid-name
+    object: str
+    created: int
+    model: str
+    choices: Sequence[Completion]
+
+    def __post_init__(self) -> None:
+        self.choices = [
+            Completion(**choice) for choice in self.choices  # type: ignore[arg-type]
+        ]
+
+
+@dataclass
+class RequestParameters:
+    model: str
+    max_tokens: int
+    temperature: float
+    logprobs: int
+
+
+@dataclass
+class Request:
+    """A request to the OpenAI API"""
+
+    parameters: RequestParameters
+    prompt: str
+
+    def submit(self) -> Reply:
+        """Submit this request to the OpenAI API and return the reply"""
+        resp = openai.Completion.create(  # type: ignore[no-untyped-call]
+            prompt=self.prompt, **asdict(self.parameters)
+        )
+        return Reply(**resp)
+
+
+def evaluate_dataset(
+    loader: DatasetLoader[P],
+    prompt_func: Callable[[Sample[P]], str],
+    results_file: Path,
+    parameters: RequestParameters,
+) -> None:
+    """Evaluate each sample of a dataset using the OpenAI API
+
+    Results will be appended to the given path.
+
+    This function will skip any samples that have already been evaluated by examining
+    the results file.
+    """
+    # Check the results file to see if we've already evaluated some of the samples
+    last_sample = find_most_recent_sample(results_file)
+
+    with jsonlines.open(results_file, mode="a", compact=True) as output:
+        for sample in loader:
+            # If we've already evaluated this sample, skip it
+            if last_sample is not None and sample.id <= last_sample:
+                continue
+
+            prompt = prompt_func(sample)
+            request = Request(parameters=parameters, prompt=prompt)
+            reply = request.submit()
+            output.write(
+                {
+                    "sample": asdict(sample),
+                    "reply": asdict(reply),
+                },
+            )
+
+
+def find_most_recent_sample(results_file: Path) -> Optional[int]:
+    """Check the results file to get the id of the sample that was last evaluated
+
+    This is helpful for resuming a long evaluation after it's been interrupted.
+
+    If the results file doesn't exist or doesn't contain any valid results, return None.
+
+    This function assumes the results are sorted by sample id.
+    """
+    if not results_file.exists():
+        return None
+
+    with jsonlines.open(results_file) as file:
+        last_line: Dict[str, Any] = {}
+        try:
+            for line in file:
+                if line:
+                    last_line = line
+        except jsonlines.InvalidLineError:
+            pass
+
+    if not last_line:
+        return None
+    last_id = last_line.get("sample", {}).get("id", None)
+    try:
+        return int(last_id)
+    except (TypeError, ValueError):
+        return None
