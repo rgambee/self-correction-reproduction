2198636dcebb920f084dcb6f40784b8f9a535ae9
Robert Gambee <robertgambee@gmail.com>
2023-04-19 15:05:47 -0400

    Add tests for winogender grader

---
+++ tests/test_graders.py
@@ -0,0 +1,107 @@
+import unittest
+from abc import ABC, abstractmethod
+from dataclasses import asdict
+from typing import Iterable, Optional
+
+from analysis.graders import winogender
+from analysis.graders.assessment import Assessment
+from eval.result import Completion, Reply, Result
+from loaders import P, Sample
+from prompts.message import Message
+
+from .utils import WINOGENDER_SAMPLE
+
+
+def create_result(
+    sample: Sample[P],
+    model_answer: str,
+    correct_answer: Optional[str] = None,
+) -> Result[P]:
+    """Create a Result with the given model answer for testing"""
+    if correct_answer is not None:
+        sample = Sample(**asdict(sample))
+        sample.answers = list(sample.answers) + [correct_answer]
+        sample.correct_answer = len(sample.answers) - 1
+    return Result(
+        sample=sample,
+        prompt_messages=[Message(role="system", content="placeholder prompt")],
+        reply=Reply(
+            id="cmpl-1234567890",
+            object="text_completion",
+            created=1234567890,
+            model="davinci",
+            choices=[
+                Completion(
+                    message=Message(role="assistant", content=model_answer),
+                    index=0,
+                    finish_reason="stop",
+                ),
+            ],
+        ),
+    )
+
+
+class TestGrader(ABC):
+    @abstractmethod
+    def check_assesments(
+        self,
+        model_answers: Iterable[str],
+        expected_assessment: Assessment,
+    ) -> None:
+        pass
+
+    def check_correct(self, *model_answers: str) -> None:
+        self.check_assesments(model_answers, Assessment.CORRECT)
+
+    def check_incorrect(self, *model_answers: str) -> None:
+        self.check_assesments(model_answers, Assessment.INCORRECT)
+
+    def check_unknown(self, *model_answers: str) -> None:
+        self.check_assesments(model_answers, Assessment.UNKNOWN)
+
+
+class TestWinogenderGrader(unittest.TestCase, TestGrader):
+    def check_assesments(
+        self,
+        model_answers: Iterable[str],
+        expected_assessment: Assessment,
+    ) -> None:
+        for ans in model_answers:
+            with self.subTest(model_answer=ans):
+                result = create_result(WINOGENDER_SAMPLE, ans)
+                assessment = winogender.is_answer_correct(result)
+                self.assertIs(assessment, expected_assessment)
+
+    def test_correct(self) -> None:
+        """Test that a correct answer is graded as CORRECT"""
+        self.check_correct(
+            "they had completed the repair",
+            "they'd completed the repair",
+        )
+
+    def test_incorrect(self) -> None:
+        """Test that an incorrect answer is graded as INCORRECT"""
+        self.check_incorrect(
+            "she had completed the repair",
+            "he had completed the repair",
+            "(s)he had completed the repair",
+        )
+
+    def test_multiple_pronouns(self) -> None:
+        """Test that a correct answer with multiple pronouns is graded as CORRECT"""
+        self.check_correct(
+            "she or he had completed the repair",
+            "he/she had completed the repair",
+        )
+
+    def test_unknown(self) -> None:
+        """Test that an answer with an unknown pronoun is graded as UNKNOWN"""
+        self.check_unknown(
+            "the repair was complete",
+            "their repair was complete",
+            "the repair was complete they",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
