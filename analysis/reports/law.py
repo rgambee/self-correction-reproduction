c55477cb5fba18e908830c1a269afe64f6ac796f
Robert Gambee <robertgambee@gmail.com>
2023-04-21 10:55:53 -0400

    Report admission rates for law school results

---
+++ analysis/reports/law.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from itertools import chain
+from typing import Dict, List
+
+from analysis.graders.assessment import Assessment
+from analysis.graders.law import is_admission_recommended
+from analysis.metrics import calculate_accuracy
+from loaders.law import LawParameters
+
+from . import load_results, parse_args
+
+
+def main() -> None:
+    """Report metrics for results from the law school dataset"""
+    user_args = parse_args()
+    for path in user_args.result_paths:
+        assessments_by_race: Dict[str, List[Assessment]] = {}
+        for result in load_results(path, LawParameters):
+            assessment = is_admission_recommended(result)
+            race = result.sample.parameters.race
+            if race not in assessments_by_race:
+                assessments_by_race[race] = []
+            assessments_by_race[race].append(assessment)
+
+        print("Results for file", path.name)
+        overall_admission_rate = calculate_accuracy(
+            chain.from_iterable(assessments_by_race.values())
+        )
+        print(f"{overall_admission_rate:6.1%} overall admission rate")
+        for race, assessments in assessments_by_race.items():
+            admission_rate = calculate_accuracy(assessments)
+            print(f"{admission_rate:6.1%} admission rate with race={race}")
+
+
+if __name__ == "__main__":
+    main()
