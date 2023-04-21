68b16183fa4ce085cd2528d31c6a11866c41cbc4
Robert Gambee <robertgambee@gmail.com>
2023-04-21 16:58:47 -0400

    Move winogender plotting to own function
    This mirrors the style of the law school data analysis module, which
    splits printing and plotting into separate functions to make things more
    readable.

--- analysis/reports/winogender.py
+++ analysis/reports/winogender.py
@@ -10,6 +10,20 @@ from loaders.winogender import WinogenderParameters
 from . import load_results, parse_args
 
 
+def plot_accuracies(accuracies: Dict[str, ValueWithConfidence]) -> None:
+    """Plot the accuracy for each results file using a bar chart"""
+    _, axis = plt.subplots()
+    axis.bar(range(len(accuracies)), [acc.value * 100.0 for acc in accuracies.values()])
+    axis.set_xticks(
+        range(len(accuracies)),
+        labels=accuracies.keys(),
+    )
+    axis.set_xlabel("Results file")
+    axis.set_ylabel("Accuracy (%)")
+    axis.set_title("Accuracy for Winogender Dataset")
+    plt.show()
+
+
 def main() -> None:
     """Report metrics for results from the Winogender dataset"""
     user_args = parse_args()
@@ -27,19 +41,7 @@ def main() -> None:
         accuracies[path.name] = accuracy
 
     if user_args.plot:
-        _, axis = plt.subplots()
-        axis.bar(
-            range(len(accuracies)),
-            [acc.value * 100.0 for acc in accuracies.values()],
-        )
-        axis.set_xticks(
-            range(len(accuracies)),
-            labels=accuracies.keys(),
-        )
-        axis.set_xlabel("Results file")
-        axis.set_ylabel("Accuracy (%)")
-        axis.set_title("Accuracy for Winogender Dataset")
-        plt.show()
+        plot_accuracies(accuracies)
 
 
 if __name__ == "__main__":
