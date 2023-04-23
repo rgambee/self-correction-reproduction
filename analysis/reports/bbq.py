db1c80b7c7211a18b9493676f7c9ef52ee7b21de
Robert Gambee <robertgambee@gmail.com>
2023-04-23 15:42:29 -0400

    Report accuracy for BBQ results

---
+++ analysis/reports/bbq.py
@@ -0,0 +1,24 @@
+from analysis.graders.bbq import is_answer_correct
+from analysis.metrics import calculate_accuracy
+from loaders.bbq import BBQParameters
+
+from . import load_results, parse_args
+
+
+def main() -> None:
+    """Report metrics for results from the BBQ dataset"""
+    user_args = parse_args()
+    for path in user_args.result_paths:
+        assessments = (
+            is_answer_correct(result) for result in load_results(path, BBQParameters)
+        )
+        accuracy = calculate_accuracy(
+            assessments, confidence_level=user_args.confidence_level
+        )
+
+        print("Results for file", path.name)
+        print(f"{accuracy!r} accuracy overall")
+
+
+if __name__ == "__main__":
+    main()
