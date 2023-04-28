295ed7e43d24ee0a61fb457e19b8ca273e3d63ef
Robert Gambee <robertgambee@gmail.com>
2023-04-28 17:46:45 -0400

    Create general Ratio class with confidence interval
    I want to represent proportions other than binomial distributions.

--- analysis/metrics/binomial.py
+++ analysis/metrics/binomial.py
@@ -4,7 +4,55 @@ from typing import List, Optional, Sequence, Tuple
 from scipy import stats
 
 
-class BinomialDistribution:
+class ValueCI:
+    """A value and associated confidence interval"""
+
+    def __init__(
+        self,
+        value: float,
+        confidence_level: float,
+        confidence_interval: Tuple[float, float],
+    ) -> None:
+        self._value = value
+        self.confidence_level = confidence_level
+        self.confidence_interval = confidence_interval
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    @property
+    def ci_low(self) -> float:
+        return float(self.confidence_interval[0])
+
+    @property
+    def ci_high(self) -> float:
+        return float(self.confidence_interval[1])
+
+    @property
+    def ci_low_rel(self) -> float:
+        return self.ci_low - self.value
+
+    @property
+    def ci_high_rel(self) -> float:
+        return self.ci_high - self.value
+
+    def __str__(self) -> str:
+        return f"{self.value:.3f}"
+
+    def __repr__(self) -> str:
+        return " ".join(
+            (
+                # Use a width of 6 so things line up nicely with BinomialDistribution
+                # strings too.
+                f"{self.value:6.3f}",
+                f"({self.confidence_level:3.0%} CI:",
+                f"{self.ci_low:.3f} - {self.ci_high:.3f})",
+            )
+        )
+
+
+class BinomialDistribution(ValueCI):
     """A binomial distribution with an associated confidence interval
 
     The interval is an absolute one:
@@ -13,6 +61,7 @@ class BinomialDistribution:
     An relative interval can be obtained with ci_low_rel and ci_high_rel.
     """
 
+    # pylint: disable-next=super-init-not-called
     def __init__(
         self,
         successes: int,
@@ -31,32 +80,26 @@ class BinomialDistribution:
             )
 
     @property
-    def proportion(self) -> float:
+    def value(self) -> float:
         return float(self.binom_test_result.statistic)
 
     @property
-    def ci_low(self) -> float:
-        return float(self.confidence_interval[0])
+    def proportion(self) -> float:
+        """An alias for self.value
 
-    @property
-    def ci_high(self) -> float:
-        return float(self.confidence_interval[1])
-
-    @property
-    def ci_low_rel(self) -> float:
-        return self.ci_low - self.proportion
-
-    @property
-    def ci_high_rel(self) -> float:
-        return self.ci_high - self.proportion
+        The term proportion is more specific and intuitive in the case of binomial
+        distributions, where we're dealing with a ratio of successes and samples.
+        """
+        return self.value
 
     def __str__(self) -> str:
-        return f"{self.proportion:6.1%}"
+        # Since self.value lies in the range [0, 1], format it as a percentage
+        return f"{self.value:6.1%}"
 
     def __repr__(self) -> str:
         return " ".join(
             (
-                f"{self.proportion:6.1%}",
+                f"{self.value:6.1%}",
                 f"({self.confidence_level:3.0%} CI:",
                 f"{self.ci_low:6.1%} - {self.ci_high:6.1%})",
             )
@@ -120,7 +163,7 @@ def binomial_difference(
 
 
 def error_bars(
-    binoms: Sequence[BinomialDistribution],
+    ratios: Sequence[ValueCI],
     multiplier: float = 100.0,
 ) -> Tuple[List[float], List[float]]:
     """Return error bars compatible with Matplotlib
@@ -131,6 +174,6 @@ def error_bars(
     return (
         # Matplotlib expects both sides of the error bar to be positive, which is why we
         # take the absolute value of the relative confidence interval.
-        [abs(b.ci_low_rel * multiplier) for b in binoms],
-        [abs(b.ci_high_rel * multiplier) for b in binoms],
+        [abs(r.ci_low_rel * multiplier) for r in ratios],
+        [abs(r.ci_high_rel * multiplier) for r in ratios],
     )
