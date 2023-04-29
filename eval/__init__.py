c3358fa6fa86a1a6dfb0202df843e30507335880
Robert Gambee <robertgambee@gmail.com>
2023-04-29 17:41:53 -0400

    Switch over to using Pipeline and Stage classes

--- eval/__init__.py
+++ eval/__init__.py
@@ -1,10 +1,11 @@
 import asyncio
 import logging
 from pathlib import Path
-from typing import Any, Callable, Iterable, List, Optional, Set
+from typing import Callable, Iterable, Set
 
 import jsonlines
 
+from eval.pipeline import Pipeline, Stage
 from eval.processing import process_requests, process_results, process_samples
 from eval.request import Request, RequestParameters
 from eval.result import Result
@@ -37,49 +38,50 @@ async def evaluate_dataset(
     if saved_samples:
         logger.info("Skipping %d previously evaluated samples", len(saved_samples))
 
+    pipeline = Pipeline()
     requests_queue: asyncio.Queue[Request[P]] = asyncio.Queue(maxsize=num_workers)
     results_queue: asyncio.Queue[Result[P]] = asyncio.Queue(maxsize=num_workers)
 
-    # Create tasks to track all the workers
-    sample_task = asyncio.create_task(
-        process_samples(
-            samples=samples,
-            prompt_func=prompt_func,
-            parameters=parameters,
-            requests_queue=requests_queue,
-            max_requests_per_min=max_requests_per_min,
-            previously_saved_samples=saved_samples,
-        ),
-    )
-    sample_task.set_name("process_samples")
-
-    request_tasks: List[asyncio.Task[None]] = []
-    for i in range(num_workers):
-        task = asyncio.create_task(
-            process_requests(
-                requests_queue=requests_queue,
-                results_queue=results_queue,
-            ),
+    # Create a pipeline to hold all the tasks
+    # Stage 0: turn Samples into Requests using prompt_func()
+    pipeline.append_stage(
+        Stage.from_coro(
+            coro_func=process_samples,
+            kwargs={
+                "samples": samples,
+                "prompt_func": prompt_func,
+                "parameters": parameters,
+                "requests_queue": requests_queue,
+                "max_requests_per_min": max_requests_per_min,
+                "previously_saved_samples": saved_samples,
+            },
+            output_queue=requests_queue,
         )
-        task.set_name(f"process_requests_{i:02}")
-        request_tasks.append(task)
-
-    result_task = asyncio.create_task(
-        process_results(
-            results_queue=results_queue,
-            results_file=results_file,
-        ),
     )
-    result_task.set_name("process_results")
-
-    await wait_for_tasks_to_complete(
-        sample_task=sample_task,
-        request_tasks=request_tasks,
-        result_task=result_task,
-        requests_queue=requests_queue,
-        results_queue=results_queue,
-        timeout=parameters.timeout,
+    # Stage 1: turn Requests into Results by submitting them to the API
+    pipeline.append_stage(
+        Stage.from_coro(
+            coro_func=process_requests,
+            kwargs={
+                "requests_queue": requests_queue,
+                "results_queue": results_queue,
+            },
+            output_queue=results_queue,
+            num_tasks=num_workers,
+        )
     )
+    # Stage 2: save Results to file
+    pipeline.append_stage(
+        Stage.from_coro(
+            coro_func=process_results,
+            kwargs={
+                "results_queue": results_queue,
+                "results_file": results_file,
+            },
+            output_queue=None,
+        )
+    )
+    await pipeline.wait(timeout=parameters.timeout)
 
 
 def get_saved_samples(results_file: Path) -> Set[int]:
@@ -103,71 +105,3 @@ def get_saved_samples(results_file: Path) -> Set[int]:
                 if sample_id is not None:
                     ids.add(sample_id)
     return ids
-
-
-# pylint: disable-next=too-many-arguments
-async def wait_for_tasks_to_complete(
-    sample_task: asyncio.Task[None],
-    request_tasks: Iterable[asyncio.Task[None]],
-    result_task: asyncio.Task[None],
-    requests_queue: asyncio.Queue[Request[P]],
-    results_queue: asyncio.Queue[Result[P]],
-    timeout: Optional[float] = None,
-) -> None:
-    """Wait for all tasks to complete and handle any errors that occur"""
-    logger = logging.getLogger(__name__)
-
-    pending_tasks = set(request_tasks)
-    pending_tasks.add(sample_task)
-    pending_tasks.add(result_task)
-
-    finished = False
-    try:
-        while not finished and pending_tasks:
-            done_tasks, pending_tasks = await asyncio.wait(
-                pending_tasks, return_when=asyncio.FIRST_COMPLETED
-            )
-            for task in done_tasks:
-                logger.debug("Task %s is done", task.get_name())
-                err = task.exception()
-                if err is not None:
-                    raise err
-                if task is sample_task:
-                    # No more samples. Break out of while loop to shut down other tasks.
-                    logger.info("No more samples, shutting down...")
-                    finished = True
-    except Exception:
-        logger.exception("Encountered error, shutting down...")
-        raise
-    finally:
-        # Shut things down from upstream to downstream to avoid information being lost
-        await stop_task(sample_task)
-        try:
-            await asyncio.wait_for(requests_queue.join(), timeout=timeout)
-        except asyncio.TimeoutError:
-            logger.debug("Timed out while waiting for pending requests to be sent")
-
-        for task in request_tasks:
-            await stop_task(sample_task)
-        if not result_task.done():
-            # The result task is still running. Give it a moment to finish saving any
-            # results still in the queue.
-            try:
-                await asyncio.wait_for(results_queue.join(), timeout=timeout)
-            except asyncio.TimeoutError:
-                logger.debug("Not all pending results were able to be saved")
-        await stop_task(result_task)
-
-
-async def stop_task(task: asyncio.Task[Any], timeout: Optional[float] = None) -> None:
-    """Stop the given task gracefully by canceling it and waiting for it to finish"""
-    logger = logging.getLogger(__name__)
-    if not task.done():
-        logger.debug("Canceling task %s", task.get_name())
-        task.cancel()
-        try:
-            await asyncio.wait_for(task, timeout=timeout)
-        except asyncio.CancelledError:
-            logger.debug("Task %s canceled", task.get_name())
-        except asyncio.TimeoutError:
-            logger.debug("Timed out while waiting for %s to cancel", task.get_name())
